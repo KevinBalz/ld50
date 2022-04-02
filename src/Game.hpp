e0ec727924b6f1bf3e774915e162107a99b4ec2f
Kevin Balz <kevin.balz@web.de>
2022-04-02 11:12:34 +0200

    Palette System

--- src/Game.hpp
+++ src/Game.hpp
@@ -3,21 +3,69 @@
 #include "OpenGLPixelArtDrawer.hpp"
 #include "World.hpp"
 #include "Renderer.hpp"
-#include "GridObject.hpp"
+#include "Grid.hpp"
 #include "Player.hpp"
+#include <time.h>
+#include <stdlib.h>
 
 class Game
 {
 public:
 	void Setup(const tako::SetupData& setup)
 	{
+		srand(time(NULL));
 		m_context = setup.context;
 		m_drawer = new tako::OpenGLPixelArtDrawer(setup.context);
 		m_drawer->Resize(1024, 768);
 		m_drawer->SetTargetSize(240, 135);
 		m_drawer->AutoScale();
 
+		ChangePalette(DefaultPalette);
+		m_tile = LoadPaletteSprite("/Tile.png");
+
 		InitWorld();
+	}
+
+	tako::Bitmap ApplyPaletteBitmap(const tako::Bitmap& bitmap, const Palette& palette)
+	{
+		auto b = bitmap.Clone();
+		for (int x = 0; x < b.Width(); x++)
+		{
+			for (int y = 0; y < b.Height(); y++)
+			{
+				auto pixel = b.GetPixel(x, y);
+				if (pixel.a == 0)
+				{
+					continue;
+				}
+				switch (pixel.r)
+				{
+					case   0: b.SetPixel(x,y, palette[0]); break;
+					case  85: b.SetPixel(x,y, palette[1]); break;
+					case 170: b.SetPixel(x,y, palette[2]); break;
+					case 255: b.SetPixel(x,y, palette[3]); break;
+				}
+			}
+		}
+		return std::move(b);
+	}
+
+	PaletteSprite* LoadPaletteSprite(std::string path)
+	{
+		auto bitmap = tako::Bitmap::FromFile(path.c_str());
+		auto paletted = ApplyPaletteBitmap(bitmap, m_activePalette);
+		m_spriteMap[path] = { std::move(bitmap), m_drawer->CreateTexture(paletted)};
+		return &m_spriteMap[path];
+	}
+
+	void ChangePalette(const Palette& palette)
+	{
+		m_activePalette = palette;
+		for (auto& [key, sprite] : m_spriteMap)
+		{
+			auto bitmap = ApplyPaletteBitmap(sprite.source, palette);
+			m_drawer->UpdateTexture(sprite.texture, bitmap);
+		}
 	}
 
 	void InitWorld()
@@ -31,12 +79,22 @@ public:
 		);
 	}
 
+	tako::Color RandomColor()
+	{
+		return tako::Color(rand() % 255, rand() % 255, rand() % 255, 255);
+	}
+
 	void Update(tako::Input* input, float dt)
 	{
+		if (input->GetKeyDown(tako::Key::Space))
+		{
+			ChangePalette({RandomColor(),RandomColor(),RandomColor(),RandomColor()});
+		}
 		m_world.IterateComps<GridObject, MovingObject, Player>([&](GridObject& grid, MovingObject& move, Player& player)
 		{
 			if (!move.IsMoving(grid))
 			{
+				//TODO: limit movement to one direction at a time
 				if (input->GetKey(tako::Key::Left) || input->GetKey(tako::Key::A) || input->GetKey(tako::Key::Gamepad_Dpad_Left))
 				{
 					move.targetX -= 1;
@@ -81,6 +139,15 @@ public:
 		auto drawer = m_drawer;
 		m_context->Begin();
 		drawer->Clear();
+		m_drawer->SetClearColor(m_activePalette[3]);
+
+		for (int x = -50; x < 50; x++)
+		{
+			for (int y = -50; y < 50; y++)
+			{
+				drawer->DrawImage(x * 8, y * 8, 8, 8, m_tile->texture.handle);
+			}
+		}
 
 		m_world.IterateComps<GridObject, RectangleRenderer>([&](GridObject& grid, RectangleRenderer& rect)
 		{
@@ -93,6 +160,9 @@ private:
 	tako::GraphicsContext* m_context = nullptr;
 	tako::OpenGLPixelArtDrawer* m_drawer;
 	tako::World m_world;
+	std::unordered_map<std::string, PaletteSprite> m_spriteMap;
+	PaletteSprite* m_tile;
+	Palette m_activePalette;
 };
 
 
--- src/Renderer.hpp
+++ src/Renderer.hpp
@@ -1,6 +1,10 @@
 #pragma once
 #include "Tako.hpp"
 
+using Palette = std::array<tako::Color, 4>;
+
+constexpr Palette DefaultPalette = {tako::Color("#081820"), tako::Color("#346856"), tako::Color("#88c070"), tako::Color("#e0f8d0")};
+
 struct RectangleRenderer
 {
 	tako::Vector2 size;
@@ -12,4 +16,14 @@ struct SpriteRenderer
 	tako::Vector2 size;
 	tako::Sprite* sprite;
 	tako::Vector2 offset;
+};
+
+struct PaletteSprite
+{
+	tako::Bitmap source;
+	tako::Texture texture;
+
+	PaletteSprite(): source(1,1){}
+
+	PaletteSprite(tako::Bitmap&& source, tako::Texture texture): source(std::move(source)), texture(texture) {}
 };
